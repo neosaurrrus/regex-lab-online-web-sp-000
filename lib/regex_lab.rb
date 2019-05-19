417728a129ac405b421b0237218309733d152fb1
Lukie Kang <lukiek+f@gmail.com>
2019-05-19 21:20:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,6 @@
 def starts_with_a_vowel?(word)
-if word == word.match(/^[aeiou]/)
-  return true
-end
+ if ( word =~ /\A[aieou]/)
+   true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
